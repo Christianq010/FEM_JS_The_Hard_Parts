a498472f9f0bc1402c6c84feb3fc612479f4486e
Christiaan Quyn <christianq010@gmail.com>
2018-07-16 18:41:02 +0530

    add solution for question 2

--- csbin.js
+++ csbin.js
@@ -9,5 +9,17 @@ function addTwo(num) {
 }
 
 // To check if you've completed it, uncomment these console.logs!
-console.log(addTwo(3));
-console.log(addTwo(10));
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+
+// Challenge 2
+function addS(word) {
+	const result = word + 's';
+  return result;
+}
+
+// uncomment these to check your work
+// console.log(addS('pizza'));
+// console.log(addS('bagel'));
+
