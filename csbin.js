092fdd8a6d67b123eb438c655da80db74ee31b0c
Christiaan Quyn <christianq010@gmail.com>
2018-07-16 19:09:57 +0530

    add callback function questions and answer

--- csbin.js
+++ csbin.js
@@ -23,3 +23,36 @@ function addS(word) {
 // console.log(addS('pizza'));
 // console.log(addS('bagel'));
 
+
+// Challenge 3
+function map(array, callback) {
+	let empty = [];
+  for (let i=0; i < array.length; i++) {
+    empty.push(addTwo(array[i]));
+  }
+  return empty;
+}
+
+
+// console.log(map([1, 2, 3], addTwo));
+
+
+// Challenge 4
+function forEach(array, callback) {
+	let output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(multiplyBy5(array[i]));
+  }
+  return output;
+  
+}
+
+function multiplyBy5 (num) {
+  return num * 5;
+}
+
+let randomNumbers = [1, 5, 10, 15];
+
+let answer = forEach(randomNumbers, multiplyBy5);
+console.log(answer);
+// see for yourself if your forEach works!
