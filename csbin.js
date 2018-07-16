547352f7b929975a6dad4f3eeda0babc4c164687
Christiaan Quyn <christianq010@gmail.com>
2018-07-16 18:40:20 +0530

    add questions from csbin code challenge

---
+++ csbin.js
@@ -0,0 +1,13 @@
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+// console.log('Hello, world!');
+
+
+// Challenge 1
+function addTwo(num) {
+	const result = num + 2;
+  return result;
+}
+
+// To check if you've completed it, uncomment these console.logs!
+console.log(addTwo(3));
+console.log(addTwo(10));
