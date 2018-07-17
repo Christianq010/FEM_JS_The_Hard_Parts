3d74791033c7ebb301998d5c4ed21f0f48a9d35c
Christiaan Quyn <christianq010@gmail.com>
2018-07-17 16:22:34 +0530

    refactor directory structure with lesson folders

---
+++ Functions/principles.js
@@ -0,0 +1,10 @@
+// Code from the lessons
+
+const num = 3;
+function multiplyBy2 (inputNumber) {
+    const result = inputNumber * 2;
+    return result;
+}
+
+const output = multiplyBy2(4);
+const newOutput = multiplyBy2(10);
